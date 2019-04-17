9ecbff5b4a19dcc9ed694c1e1174f96bbecc24d5
Gabriel <amaning2@illinois.edu>
2019-04-17 02:03:09 -0500

    commit

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -8,6 +8,253 @@ const twdata6 = require('../../data/twData/trump_tweets');
 const fbdata = require('../../data/oldFbdata/fbdata.json');
 const fbdata1 = require('../../data/oldFbdata/fbdata1.json');
 
+let twitter_count = 0; //number of tweets generated
+document.getElementById("number_of_twitter_results").innerHTML = twitter_count + ' results';
+let facebook_count = 0; //number of facebook comments generated
+document.getElementById("number_of_facebook_results").innerHTML = facebook_count + ' results';
+let reddit_count = 0; //number of reddit posts generated
+document.getElementById("number_of_reddit_results").innerHTML = reddit_count + ' results';
+let total_count = 0; //total number of tweets, comments and posts generated
+document.getElementById("number_of_results").innerHTML = total_count + ' results';
+
+/* css-styled html for each tweet result that appears on the page */
+function tweet_html(html) {
+    return '<div class="tweet">' +
+    '<i class="fab fa-twitter"></i>' +
+    '<br>' +
+    '<div class="tweet_text_box">' +
+    '<p class="tweet_text">' + html.rawText + '</p>' +
+    '</div>' +
+    '<br>' +
+    '</div>' +
+    '<br>';
+}
+
+/* tweet output (html, results numbers) generated from filtering data */
+function twitter_output(output) {
+    document.getElementById("Twitter").innerHTML = output;
+    document.getElementById("number_of_twitter_results").innerHTML = twitter_count + ' results';
+    total_count += twitter_count;
+    document.getElementById("number_of_results").innerHTML = total_count + ' results';
+}
+
+/* returns tweets and result numbers for any tweet that contains the chosen input as a substring */
+function twitter_results(typed_input) {
+    let output = "";
+    function readTweetData(data) {
+        for (let tweet of data) {
+            if(tweet.rawText != null) {
+                if(tweet.rawText.toUpperCase().includes(typed_input.toUpperCase())) {
+                     twitter_count++;
+                     output += tweet_html(tweet);
+                }
+            }
+        }
+    }
+
+    readTweetData(twdata);
+    readTweetData(twdata1);
+    readTweetData(twdata2);
+    readTweetData(twdata3);
+    readTweetData(twdata4);
+    readTweetData(twdata5);
+    readTweetData(twdata6);
+    twitter_output(output);
+}
+
+/* returns positive sentiment tweets and result numbers for any tweet that contains the chosen input as a substring */
+function twitter_positive_results(typed_input) {
+    let output = "";
+    function readPositiveTweetData(data) {
+        for (let tweet of data) {
+            if(tweet.rawText != null) {
+                if(tweet.polarity != null) {
+                    if(tweet.polarity > 0 && tweet.rawText.toUpperCase().includes(typed_input.toUpperCase())) {
+                        twitter_count++;
+                        output += tweet_html(tweet);
+                    }
+                }
+            }
+        }
+    }
+
+    readPositiveTweetData(twdata);
+    readPositiveTweetData(twdata1);
+    readPositiveTweetData(twdata2);
+    readPositiveTweetData(twdata3);
+    readPositiveTweetData(twdata4);
+    readPositiveTweetData(twdata5);
+    readPositiveTweetData(twdata6);
+    twitter_output(output);
+}
+
+/* returns negative sentiment tweets and result numbers for any tweet that contains the chosen input as a substring */
+function twitter_negative_results(typed_input) {
+    let output = "";
+    function readNegativeTweetData(data) {
+        for (let tweet of data) {
+            if(tweet.rawText != null) {
+                if(tweet.polarity != null) {
+                    if(tweet.polarity < 0 && tweet.rawText.toUpperCase().includes(typed_input.toUpperCase())) {
+                        twitter_count++;
+                        output += tweet_html(tweet);
+                    }
+                }
+            }
+        }
+    }
+
+    readNegativeTweetData(twdata);
+    readNegativeTweetData(twdata1);
+    readNegativeTweetData(twdata2);
+    readNegativeTweetData(twdata3);
+    readNegativeTweetData(twdata4);
+    readNegativeTweetData(twdata5);
+    readNegativeTweetData(twdata6);
+    twitter_output(output);
+}
+
+/* returns neutral sentiment tweets and result numbers for any tweet that contains the chosen input as a substring */
+function twitter_neutral_results(typed_input) {
+    let output = "";
+    function readNeutralTweetData(data) {
+        for (let tweet of data) {
+            if(tweet.rawText != null) {
+                if(tweet.polarity != null) {
+                    if(tweet.polarity === 0 && tweet.rawText.toUpperCase().includes(typed_input.toUpperCase())) {
+                        twitter_count++;
+                        output += tweet_html(tweet);
+                    }
+                }
+            }
+        }
+    }
+
+    readNeutralTweetData(twdata);
+    readNeutralTweetData(twdata1);
+    readNeutralTweetData(twdata2);
+    readNeutralTweetData(twdata3);
+    readNeutralTweetData(twdata4);
+    readNeutralTweetData(twdata5);
+    readNeutralTweetData(twdata6);
+    twitter_output(output);
+}
+
+
+/* css-styled html for each facebook comment result that appears on the page */
+function fb_comment_html(html) {
+    return '<div class="fb_comment">' +
+        '<i class="fab fa-facebook"></i>' +
+        '<br>' +
+        '<div class="fb_comment_text_box">' +
+        '<p class="fb_comment_text">' + html.Comment + '</p>' +
+        '</div>' +
+        '<br>' +
+        '</div>' +
+        '<br>';
+}
+
+/* comment output (html, results numbers) generated from filtering data */
+function facebook_output(output) {
+    document.getElementById("Facebook").innerHTML = output;
+    document.getElementById("number_of_facebook_results").innerHTML = facebook_count + ' results';
+    total_count += facebook_count;
+    document.getElementById("number_of_results").innerHTML = total_count + ' results';
+}
+
+/* returns comments and result numbers for any tweet that contains the chosen input as a substring */
+function facebook_results(typed_input) {
+    let output = "";
+    function readFbData(data) {
+        for (let post of data) {
+            if(post.Comment != null) {
+                if(post.Comment.toUpperCase().includes(typed_input.toUpperCase())) {
+                    facebook_count++;
+                    output += fb_comment_html(post);
+                }
+            }
+        }
+    }
+
+    readFbData(fbdata);
+    readFbData(fbdata1);
+    facebook_output(output);
+}
+
+/* returns positive sentiment comments and result numbers for any comment that contains the chosen input as a substring */
+function facebook_positive_results(typed_input) {
+    let output = "";
+    function readPostiveFbData(data) {
+        for (let post of data) {
+            if(post.Comment != null) {
+                if(post.Positive != null && post.Negative != null) {
+                    if(post.Positive > post.Negative && post.Comment.toUpperCase().includes(typed_input.toUpperCase())) {
+                        facebook_count++;
+                        output += fb_comment_html(post);
+                    }
+                }
+            }
+        }
+    }
+
+    readPostiveFbData(fbdata);
+    readPostiveFbData(fbdata1);
+    facebook_output(output);
+}
+
+/* returns negative sentiment comments and result numbers for any comment that contains the chosen input as a substring */
+function facebook_negative_results(typed_input) {
+    let output = "";
+    function readNegativeFbData(data) {
+        for (let post of data) {
+            if(post.Comment != null) {
+                if(post.Positive != null && post.Negative != null) {
+                    if(post.Positive < post.Negative && post.Comment.toUpperCase().includes(typed_input.toUpperCase())) {
+                        facebook_count++;
+                        output += fb_comment_html(post);
+                    }
+                }
+            }
+        }
+    }
+
+    readNegativeFbData(fbdata);
+    readNegativeFbData(fbdata1);
+    facebook_output(output);
+}
+
+
+/* clears html and resets result numbers/counts to 0 */
+function clear() {
+    document.getElementById("Twitter").innerHTML = '';
+    twitter_count = 0;
+    document.getElementById("number_of_twitter_results").innerHTML = twitter_count + ' results';
+    document.getElementById("Facebook").innerHTML = '';
+    facebook_count = 0;
+    document.getElementById("number_of_facebook_results").innerHTML = facebook_count + ' results';
+    total_count = 0;
+    document.getElementById("number_of_results").innerHTML = total_count + ' results';
+}
+
+/* enables search after typing in input and clicking on the search button */
+document.getElementById('search_button').onclick=function() {
+    clear();
+    let typed_input = document.getElementById("search_input").value;
+    twitter_results(typed_input);
+    openTab(event, 'Twitter');
+    facebook_results(typed_input)
+};
+
+/* enables search from pressing the enter key */
+let input = document.getElementById("search_input");
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("search_button").click();
+    }
+});
+
+/* allows for switching through tabs */
 function openTab(event, siteName) {
     let i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -21,99 +268,33 @@ function openTab(event, siteName) {
     document.getElementById(siteName).style.display = "block";
     event.currentTarget.className += " active";
 }
-
-let total_count = 0;
-document.getElementById("number_of_results").innerHTML = total_count + ' results';
-let twitter_count = 0;
-document.getElementById("number_of_twitter_results").innerHTML = twitter_count + ' results';
-let facebook_count = 0;
-document.getElementById("number_of_facebook_results").innerHTML = facebook_count + ' results';
-let reddit_count = 0;
-document.getElementById("number_of_reddit_results").innerHTML = reddit_count + ' results';
-
-function twitter_results() {
-    let stdin = document.getElementById("search_input").value;
-    let output = "";
-    function readTweetData(data) {
-        for (let tweet of data) {
-            if(tweet.rawText != null) {
-                if(tweet.rawText.toUpperCase().includes(stdin.toUpperCase())) {
-                     twitter_count++;
-                     output +=
-                     '<div class="tweet">' +
-                     '<i class="fab fa-twitter"></i>' +
-                     '<br>' +
-                     '<div class="tweet_text_box">' +
-                     '<p class="tweet_text">' + tweet.rawText + '</p>' +
-                     '</div>' +
-                     '<br>' +
-                     '</div>' +
-                     '<br>';
-                }
-            }
-        }
-    }
-
-    readTweetData(twdata);
-    readTweetData(twdata1);
-    readTweetData(twdata2);
-    readTweetData(twdata3);
-    readTweetData(twdata4);
-    readTweetData(twdata5);
-    readTweetData(twdata6);
-
-    document.getElementById("Twitter").innerHTML = output;
-    document.getElementById("number_of_twitter_results").innerHTML = twitter_count + ' results';
-    total_count += twitter_count;
-    document.getElementById("number_of_results").innerHTML = total_count + ' results';
-}
-
-function facebook_results() {
-    let stdin = document.getElementById("search_input").value;
-    let output = "";
-    function readFbData(data) {
-        for (let post of data) {
-            if(post.Comment != null) {
-                if(post.Comment.toUpperCase().includes(stdin.toUpperCase())) {
-                    facebook_count++;
-                    output +=
-                        '<div class="fb_comment">' +
-                        '<i class="fab fa-facebook"></i>' +
-                        '<br>' +
-                        '<div class="fb_comment_text_box">' +
-                        '<p class="fb_comment_text">' + post.Comment + '</p>' +
-                        '</div>' +
-                        '<br>' +
-                        '</div>' +
-                        '<br>';
-                }
-            }
-        }
-    }
-
-    readFbData(fbdata);
-    readFbData(fbdata1);
-
-    document.getElementById("Facebook").innerHTML = output;
-    document.getElementById("number_of_facebook_results").innerHTML = facebook_count + ' results';
-    total_count += facebook_count;
-    document.getElementById("number_of_results").innerHTML = total_count + ' results';
-}
-
-document.getElementById('search_button').onclick=function() {
-    twitter_results();
-    openTab(event, 'Twitter');
-    facebook_results()
-}; //search after clicking on button
-
-let input = document.getElementById("search_input");
-input.addEventListener("keyup", function(event) { //search after pressing enter
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        document.getElementById("search_button").click();
-    }
-});
-
 document.getElementById('twitter_tab').onclick=function() {openTab(event, 'Twitter');};
 document.getElementById('facebook_tab').onclick=function() {openTab(event, 'Facebook');};
 document.getElementById('reddit_tab').onclick=function() {openTab(event, 'Reddit');};
+
+
+/* filters search results by positive sentimentality on form press */
+document.getElementById('positive').onchange=function(){
+    clear();
+    let typed_input = document.getElementById("search_input").value;
+    twitter_positive_results(typed_input);
+    openTab(event, 'Twitter');
+    facebook_positive_results(typed_input)
+};
+
+/* filters search results by negative sentimentality on form press */
+document.getElementById('negative').onchange=function(){
+    clear();
+    let typed_input = document.getElementById("search_input").value;
+    twitter_negative_results(typed_input);
+    openTab(event, 'Twitter');
+    facebook_negative_results(typed_input)
+};
+
+/* filters search results by neutral sentimentality on form press */
+document.getElementById('neutral').onchange=function(){
+    clear();
+    let typed_input = document.getElementById("search_input").value;
+    twitter_neutral_results(typed_input);
+    openTab(event, 'Twitter')
+};
--- source/scripts/plot.py
+++ source/scripts/plot.py
@@ -2,31 +2,141 @@ import json
 import matplotlib.pylab as plt
 
 #  ------- Gathering data -------  #
-path = 'C:/Users/gabeamaning/IdeaProjects/sp-19-cs242-final-project/data/twdata/'
-with open(path + 'booker_tweets.json') as plot_data:
-    data = json.load(plot_data)
-with open(path + 'christie_tweets.json') as plot_data:
-    data1 = json.load(plot_data)
-with open(path + 'clinton_tweets.json') as plot_data:
-    data2 = json.load(plot_data)
-with open(path + 'cruz_tweets.json') as plot_data:
-    data3 = json.load(plot_data)
-with open(path + 'obama_tweets.json') as plot_data:
-    data4 = json.load(plot_data)
-with open(path + 'sanders_tweets.json') as plot_data:
-    data5 = json.load(plot_data)
-with open(path + 'trump_tweets.json') as plot_data:
-    data6 = json.load(plot_data)
+path = 'C:/Users/gabeamaning/IdeaProjects/sp-19-cs242-final-project/data/'
+with open(path + 'twdata/booker_tweets.json') as plot_data:
+    booker_tweet_data = json.load(plot_data)
+with open(path + 'twdata/christie_tweets.json') as plot_data:
+    christie_tweet_data = json.load(plot_data)
+with open(path + 'twdata/clinton_tweets.json') as plot_data:
+    clinton_tweet_data = json.load(plot_data)
+with open(path + 'twdata/cruz_tweets.json') as plot_data:
+    cruz_tweet_data = json.load(plot_data)
+with open(path + 'twdata/obama_tweets.json') as plot_data:
+    obama_tweet_data = json.load(plot_data)
+with open(path + 'twdata/sanders_tweets.json') as plot_data:
+    sanders_tweet_data = json.load(plot_data)
+with open(path + 'twdata/trump_tweets.json') as plot_data:
+    trump_tweet_data = json.load(plot_data)
 
-data = data + data1 + data2 + data3 + data4 + data5 + data6
+tweet_data = booker_tweet_data + christie_tweet_data + clinton_tweet_data + cruz_tweet_data + obama_tweet_data + sanders_tweet_data + trump_tweet_data
+
+with open(path + 'oldFbData/fbdata.json', encoding="utf8") as plot_data:
+    fbdata0 = json.load(plot_data)
+
+with open(path + 'oldFbData/fbdata1.json', encoding="utf8") as plot_data:
+    fbdata1 = json.load(plot_data)
+
+fbdata = fbdata0 + fbdata1
+
+
+#  ------- Analyzing Twitter Data -------  #
+def twitter_sentiment_analysis(data, politician):
+    table = dict([])
+    table["Positive"] = 0
+    table["Neutral"] = 0
+    table["Negative"] = 0
+    for tweet in data:
+        try:
+            if tweet['polarity'] > 0:
+                table["Positive"] += 1
+            elif tweet['polarity'] < 0:
+                table["Negative"] += 1
+            else:
+                table["Neutral"] += 1
+        except:
+            continue
+    x_axis = table.keys()
+    y_axis = table.values()
+    print(politician + ": ")
+    print(table)
+    plt.title("Twitter Sentiment Analysis Data on " + politician)
+    plt.bar(x_axis, y_axis)
+    plt.show()
+
+
+print("TWITTER SENTIMENT ANALYSIS")
+twitter_sentiment_analysis(booker_tweet_data, "Booker")
+twitter_sentiment_analysis(christie_tweet_data, "Christie")
+twitter_sentiment_analysis(clinton_tweet_data, "Clinton")
+twitter_sentiment_analysis(cruz_tweet_data, "Cruz")
+twitter_sentiment_analysis(obama_tweet_data, "Obama")
+twitter_sentiment_analysis(sanders_tweet_data, "Sanders")
+twitter_sentiment_analysis(trump_tweet_data, "Trump")
+
+
+#  ------- Analyzing Facebook Data -------  #
+def facebook_sentiment_analysis(data):
+    table = dict([])
+    table["Positive"] = 0
+    table["Negative"] = 0
+    for comment in data:
+        try:
+            if comment["Positive"] > comment["Negative"]:
+                table["Positive"] += 1
+            elif comment["Positive"] < comment["Negative"]:
+                table["Negative"] += 1
+        except:
+            continue
+    x_axis = table.keys()
+    y_axis = table.values()
+    print(table)
+    plt.title("Facebook Sentiment Analysis Data")
+    plt.bar(x_axis, y_axis)
+    plt.show()
+
+
+print("FACEBOOK SENTIMENT ANALYSIS")
+facebook_sentiment_analysis(fbdata)
+
+
+#  ------- Analyzing All Data -------  #
+#  ------- Sentiment Analysis on All Data -------  #
+def sentiment_analysis(tw_data, fb_data):
+    table = dict([])
+    table["Positive"] = 0
+    table["Negative"] = 0
+
+    for tweet in tw_data:
+        try:
+            if tweet['polarity'] > 0:
+                table["Positive"] += 1
+            elif tweet['polarity'] < 0:
+                table["Negative"] += 1
+        except:
+            continue
+
+    for comment in fb_data:
+        try:
+            if comment["Positive"] > comment["Negative"]:
+                table["Positive"] += 1
+            elif comment["Positive"] < comment["Negative"]:
+                table["Negative"] += 1
+        except:
+            continue
+    x_axis = table.keys()
+    y_axis = table.values()
+    print(table)
+    plt.title("Sentiment Analysis Data")
+    plt.bar(x_axis, y_axis)
+    plt.show()
+
+
+print("COMBINED SENTIMENT ANALYSIS")
+sentiment_analysis(tweet_data, fbdata)
 
 
 #  ------- Determining which politicians get the most mentions -------  #
 def count_mentions(query):
     count = 0
-    for tweet in data:
+    for tweet in tweet_data:
         try:
             if query.upper() in tweet['rawText'].upper():
+                count += 1
+        except:
+            continue
+    for comment in fbdata:
+        try:
+            if query.upper() in comment['Comment'].upper():
                 count += 1
         except:
             continue
@@ -36,5 +146,6 @@ def count_mentions(query):
 x = ["Booker", "Christie", "Clinton", "Cruz", "Obama", "Sanders", "Trump"]
 y = [count_mentions(x[0]), count_mentions(x[1]), count_mentions(x[2]), count_mentions(x[3]), count_mentions(x[4]), count_mentions(x[5]), count_mentions(x[6])]
 print(dict(zip(x, y)))
+plt.title("Politicians by mentions")
 plt.bar(x, y)
 plt.show()
